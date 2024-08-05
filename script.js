d1c4ad0d1015d67e9e926a1b5b414f5159fa5a9d
Pedro Mauricio De Almeida Chagas <114452732+PedroMauricio36@users.noreply.github.com>
2024-08-05 15:05:37 -0300

    Add files via upload
    Feat: Adicionando JavaScript

---
+++ script.js
@@ -0,0 +1,36 @@
+let gr1 = document.getElementById("gr1").value;;
+let gr3 = document.getElementById("gr1").value;
+let gr4 = document.getElementById("gr1").value;
+let gr5 = document.getElementById("gr1").value;
+let gr6 = document.getElementById("gr1").value;
+let ata = document.getElementById("gr1").value;
+let telefone = document.getElementById("gr1").value;
+let tv = document.getElementById("gr1").value;
+let camera = document.getElementById("gr1").value;
+let onu = document.getElementById("gr1").value;
+let ont = document.getElementById("gr1").value;
+let ponto = document.getElementById("gr1").value;
+let conversor = document.getElementById("gr1").value;
+let switchHub = document.getElementById("gr1").value;
+
+function enviar() {
+    gr1 = document.getElementById("gr1").value;
+    gr3 = document.getElementById("gr1").value;
+    gr4 = document.getElementById("gr1").value;
+    gr5 = document.getElementById("gr1").value;
+    gr6 = document.getElementById("gr1").value;
+    ata = document.getElementById("gr1").value;
+    telefone = document.getElementById("gr1").value;
+    tv = document.getElementById("gr1").value;
+    camera = document.getElementById("gr1").value;
+    onu = document.getElementById("gr1").value;
+    ont = document.getElementById("gr1").value;
+    ponto = document.getElementById("gr1").value;
+    conversor = document.getElementById("gr1").value;
+    switchHub = document.getElementById("gr1").value;
+
+    console.log(gr1);
+    
+
+}
+
